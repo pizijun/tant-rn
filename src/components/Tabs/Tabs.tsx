b1ca56683f651adf9ad99a2fa31584ce509e1802
zejun <zejuncode@163.com>
2021-02-21 18:03:17 +0800

    修复 tabbar  滚动至容器可视区域

--- src/components/Tabs/Tabs.tsx
+++ src/components/Tabs/Tabs.tsx
@@ -1,5 +1,6 @@
 import React, { FC, useEffect, useRef, useState } from "react";
-import { ScrollView, View, Text, StyleSheet, Dimensions, StyleProp, TextStyle, ViewStyle, Animated, TouchableOpacity, LayoutChangeEvent } from "react-native";
+import { ScrollView, View, Text, StyleSheet, Dimensions, StyleProp, TextStyle, ViewStyle, Animated, TouchableOpacity, LayoutChangeEvent, TouchableWithoutFeedback, } from "react-native";
+import { getRect } from "../../utils";
 import flexbem from "../Style/flexbem";
 
 const winWidth = Dimensions.get('window').width;
@@ -27,6 +28,8 @@ export const Tabs: FC<TabsProps> = (props) => {
   const [containerWidth, setContainerWidth] = useState(winWidth);
   const scrollTabBarEl = useRef(null);
   const scrollTabView = useRef(null);
+  const scrollbarWrapEl = useRef(null);
+
   const swipeThreshold = 5;
 
   const {
@@ -41,6 +44,7 @@ export const Tabs: FC<TabsProps> = (props) => {
 
   const canScroll = tabs.length > swipeThreshold;
 
+  // tab click
   const handleOnPress = (index: number) => {
     onTabClick && onTabClick(tabs[index], index);
     goToTab(index);
@@ -73,8 +77,30 @@ export const Tabs: FC<TabsProps> = (props) => {
     });
   };
 
+  const getAllRect = (nodes: any[]) => {
+    return Promise.all(nodes.map(node => getRect(node)));
+  };
+
+  // scroll tab bar into view
+  const scrollIntoView = (currentIndex: number) => {
+    Promise.all([getAllRect(scrollbarWrapEl.current._children), getRect(scrollTabBarEl.current)]).then(([tabbarItemRects, scrollbarRect]) => {
+      const tabRect = tabbarItemRects[currentIndex];
+      const offsetLeft = tabbarItemRects.slice(0, currentIndex).reduce((prev, cur) => prev + cur.width, 0);
+      requestAnimationFrame(() => {
+        scrollTabBarEl.current.scrollTo({
+          x: offsetLeft - (scrollbarRect.width - tabRect.width) / 2,
+          y: 0,
+          animated: true,
+        });
+      });
+    });
+  };
+
   useEffect(() => {
     setCurrentIndex(+page);
+    requestAnimationFrame(() => {
+      scrollIntoView(+page);
+    });
   }, [page]);
 
   return (
@@ -94,9 +120,9 @@ export const Tabs: FC<TabsProps> = (props) => {
           contentContainerStyle={[!canScroll && {flex: 1}]}
         >
           <View style={[!canScroll && {width: '100%'}]}>
-            <View style={[flexbem.flexRow]}>
+            <View style={[flexbem.flexRow]} ref={scrollbarWrapEl}>
               {tabs.map((item, i) => (
-                <TouchableOpacity key={i} activeOpacity={1} style={[flexbem.flexItem, styles.tabItem, flexbem.flexXCenter, { paddingHorizontal: canScroll ? 12: 2}, currentIndex === i && {borderBottomWidth: 2, borderBottomColor: '#5977FF'}]} onPress={() => handleOnPress(i)}>
+                <TouchableOpacity key={i} activeOpacity={1} style={[flexbem.flexItem, styles.tabItem, flexbem.flexXCenter, { paddingHorizontal: canScroll ? 12: 2}, currentIndex === i && {borderBottomWidth: 2, borderBottomColor: '#5977FF'}, canScroll && {minWidth: 50, maxWidth: 140}]} onPress={() => handleOnPress(i)}>
                   <View>
                     <Text style={[{color: currentIndex === i ? tabBarActiveTextColor : tabBarInactiveTextColor }, styles.textTab]} ellipsizeMode="tail" numberOfLines={1}>{item.title}</Text>
                   </View>
@@ -116,6 +142,7 @@ export const Tabs: FC<TabsProps> = (props) => {
               onChange(tabs[currentIndex], currentIndex);
             }
             setCurrentIndex(index);
+            scrollIntoView(index);
           }}
           horizontal={true} pagingEnabled={true} contentContainerStyle={{ flexDirection: 'row' }} showsHorizontalScrollIndicator={false}>
           {(children || []).map((child, childIndex) => (
